3dc5d0c512b1a038ba24bf420e43665735ac92c1
aaaa831108 <69152875+aaaa831108@users.noreply.github.com>
2021-02-07 21:43:21 +0800

    add count

--- guess.py
+++ guess.py
@@ -5,14 +5,18 @@
 
 import random
 
-r = random.randint(1,100) #產生1~100(包含)的隨機整數
+r = random.randint(1,100) # 產生1~100(包含)的隨機整數
+count = 0
 while True:
+    count += 1 # count = count + 1
     num = input("請猜數字: ")
     num = int(num)
     if num == r:
         print('你猜中了!')
+        print('這是你猜的', count, '次')
         break
     elif num >r:
         print('比答案大')
     elif num <r:
         print('比答案小')
+    print('這是你猜的', count, '次')
